546ab0d9f8e106ebec36d2461543f2474937a951
Anand Chowdhary <mail@anandchowdhary.com>
2020-08-09 20:48:28 +0530

    :tada: Init Svelte/Sapper site

---
+++ site/src/components/Nav.svelte
@@ -0,0 +1,63 @@
+<script>
+  export let segment;
+</script>
+
+<style>
+  nav {
+    border-bottom: 1px solid rgba(255, 62, 0, 0.1);
+    font-weight: 300;
+    padding: 0 1em;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+  }
+
+  ul::after {
+    content: "";
+    display: block;
+    clear: both;
+  }
+
+  li {
+    display: block;
+    float: left;
+  }
+
+  [aria-current] {
+    position: relative;
+    display: inline-block;
+  }
+
+  [aria-current]::after {
+    position: absolute;
+    content: "";
+    width: calc(100% - 1em);
+    height: 2px;
+    background-color: rgb(255, 62, 0);
+    display: block;
+    bottom: -1px;
+  }
+
+  a {
+    text-decoration: none;
+    padding: 1em 0.5em;
+    display: block;
+  }
+</style>
+
+<nav>
+  <ul>
+    <li>
+      <a aria-current={segment === undefined ? 'page' : undefined} href=".">
+        home
+      </a>
+    </li>
+    <li>
+      <a aria-current={segment === 'about' ? 'page' : undefined} href="about">
+        about
+      </a>
+    </li>
+  </ul>
+</nav>
