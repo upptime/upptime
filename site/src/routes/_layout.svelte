334bcbb49b1521f1b9ba1e8f21b8f55c1d1f1a18
Anand Chowdhary <mail@anandchowdhary.com>
2020-08-10 13:59:29 +0530

    :lipstick: Add footer

--- site/src/routes/_layout.svelte
+++ site/src/routes/_layout.svelte
@@ -1,11 +1,30 @@
 <script>
   import Nav from "../components/Nav.svelte";
-
+  import config from "../data/config.json";
   export let segment;
 </script>
+
+<style>
+  footer {
+    text-align: center;
+    opacity: 0.75;
+    margin-top: 3rem;
+  }
+</style>
 
 <Nav {segment} />
 
 <main class="container">
   <slot />
 </main>
+
+<footer>
+  <p>
+    This page is
+    <a href={`https://github.com/${config.owner}/${config.repo}`}>
+      open source
+    </a>
+    and powered by
+    <a href="https://up.js.org">Up.js</a>
+  </p>
+</footer>
