924e42cde8a1ae682f07b1699cf5184079f8f2e7
Anand Chowdhary <mail@anandchowdhary.com>
2020-08-14 00:58:53 +0530

    :sparkles: Show response time graph

--- site/src/components/Graph.svelte
+++ site/src/components/Graph.svelte
@@ -3,6 +3,7 @@
   import { Octokit } from "@octokit/rest";
   import { onMount } from "svelte";
   import config from "../data/config.json";
+  import Line from "svelte-chartjs/src/Line.svelte";
 
   export let slug;
   let loading = true;
@@ -12,6 +13,7 @@
   const owner = config.owner;
   const repo = config.repo;
   let commits = [];
+  let labels = [];
   let data = [];
 
   onMount(async () => {
@@ -19,7 +21,8 @@
       await octokit.repos.listCommits({
         owner,
         repo,
-        path: `history/${slug}.yml`
+        path: `history/${slug}.yml`,
+        per_page: 28,
       })
     ).data;
     commits = commits.map((commit, index) => {
@@ -30,21 +33,30 @@
       return commit;
     });
     data = commits
-      .filter(commit => commit.commit.message.includes("ms) [skip ci]"))
-      .map((commit) => parseInt(commit.commit.message.split(" in ")[1].split("ms")[0]));
+      .filter((commit) => commit.commit.message.includes("ms) [skip ci]"))
+      .map((commit) =>
+        parseInt(commit.commit.message.split(" in ")[1].split("ms")[0])
+      );
+    labels = commits
+      .filter((commit) => commit.commit.message.includes("ms) [skip ci]"))
+      .map((commit) => new Date(commit.commit.committer.date).toLocaleString());
     loading = false;
   });
 </script>
 
 <style>
+
 </style>
 
 <section>
   {#if loading}
     <Loading />
   {:else if data.length}
-    {#each data as item}
-      {JSON.stringify(item)}
-    {/each}
+    <h2>{config.i18n.sevelDayResponseTime}</h2>
+    <Line
+      data={{ labels, datasets: [{ label: config.i18n.responseTimeMs, backgroundColor: '#89e0cf', borderColor: '#1abc9c', data }] }}
+      width={200}
+      height={100}
+      options={{ responsive: true, maintainAspectRatio: true, scales: { xAxes: [{ display: false, gridLines: { display: false } }] } }} />
   {/if}
 </section>
